7610ef80db95e0a16acae0f94253c2ed558df376
Cristiano Betta <cristiano@betta.io>
2018-10-15 15:17:03 +0100

    Always show header

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,15 +3,11 @@ import { h } from "preact";
 import Typography from "preact-material-components/Typography";
 import "preact-material-components/Typography/style.css";
 
-const Header = ({ className, submitted }) => (
-  <div>
-    { !submitted && (
-      <div className={className}>
-        <Typography headline6>
-          <strong>Is this documentation useful?</strong>
-        </Typography>
-      </div>
-    )}
+const Header = ({ className }) => (
+  <div className={className}>
+    <Typography headline6>
+      <strong>Is this documentation useful?</strong>
+    </Typography>
   </div>
 );
 
