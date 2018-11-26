7919197c1431edf636e46248d2347eae62b996fb
Cristiano Betta <cbetta@box.com>
2018-11-26 16:09:42 +0000

    Add link to product feedback in widget

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,6 +6,9 @@ const Header = ({ className }) => (
     <Typography variant="h6">
       Is this documentation useful?
     </Typography>
+    <Typography variant="caption">
+      Or, <a href='https://box.uservoice.com/' target="_none"> provide us with product feedback</a>
+    </Typography>
   </div>
 );
 
